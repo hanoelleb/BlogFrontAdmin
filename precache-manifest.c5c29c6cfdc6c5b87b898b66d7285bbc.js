929310c6490d155fb912f9c0f3e468b3e59f633c
hanoelleb <hanoelleb@gmail.com>
2020-07-22 20:12:27 -0700

    Updates

---
+++ precache-manifest.c5c29c6cfdc6c5b87b898b66d7285bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6916a849a21a97125b7244d74718839",
+    "url": "/hanoelleb/BlogFrontAdmin/index.html"
+  },
+  {
+    "revision": "39d2302029372ee9a7b6",
+    "url": "/hanoelleb/BlogFrontAdmin/static/css/main.5ad99a02.chunk.css"
+  },
+  {
+    "revision": "21b83a5691796ddf0648",
+    "url": "/hanoelleb/BlogFrontAdmin/static/js/2.9cf1b434.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hanoelleb/BlogFrontAdmin/static/js/2.9cf1b434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d2302029372ee9a7b6",
+    "url": "/hanoelleb/BlogFrontAdmin/static/js/main.4ae65232.chunk.js"
+  },
+  {
+    "revision": "a32e30b08cf4854ca0bc",
+    "url": "/hanoelleb/BlogFrontAdmin/static/js/runtime-main.e328b89f.js"
+  }
+]);
